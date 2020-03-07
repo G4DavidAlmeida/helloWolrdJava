35916e7238db3dbbd0be3ab7ca18bb010b74fe8c
David Almeida <G4DavidAlmeida>
2020-03-06 21:40:53 -0300

    explore variables

--- olaMundoJava.java
+++ olaMundoJava.java
@@ -1,6 +1,21 @@
 public class olaMundoJava
 {
     public static void main(String[] args) {
-        System.out.println("Meu primeiro codigo em java na Fib");
+        double valorSalario = 1300.45; // valores decimais
+        
+        int numeroDenpendentes = 5; // valores inteiros
+        
+        String nomeCidade = "Bauru"; // valores string
+        
+        char letra = 'B'; // valores caracteres
+        
+        boolean temSaldo = true; //  true ou false
+        
+        double valor1 = 10.20;
+        double valor2 = 30.20;
+        double soma = valor1 + valor2;
+        
+        System.out.println("A soma é " + soma);
+        System.out.println(System.out);
     }
 }
